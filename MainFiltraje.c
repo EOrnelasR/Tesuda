23e9a4a81543d8ab9a2d25797f16644600a13c88
EOrnelasR <91753324+EOrnelasR@users.noreply.github.com>
2021-11-03 23:05:04 -0600

    Update MainFiltraje.c

--- MainFiltraje.c
+++ MainFiltraje.c
@@ -27,19 +27,38 @@ Asginacion de Funciones
 */
 void config(void){
   TRISBbits.RB0 = 0;
-}  
-//////////////////////////////////////////////////////////
+}
 
-void main(void){
-  config();
+void InitADC(void){
   ADC_Init();
   ADC_ON();
   ADC_AQ();
   ADCON0bits.GO_DONE = 1;
+}
+
+void __interrupt() INT_isr(void){
+  InitADC();
   while(ADCON0bits.GO_DONE == 1){
     valor = ADRES;
     voltaje = (5.0*valor)/1023;
-  } 
+  }
+  if(PIR1bits.ADIF == 1){
+    ADCON0bits.ADON = 0; 
+  }
+  __delay_ms(1000);
+  
+  INTCONbits.INT0IF = 0;
+}
+//////////////////////////////////////////////////////////
+
+void main(void){
+  config();
+  while(1){
+     LATBbits.RB0 = 0;
+     __delay_ms(1000);
+    LATBbits.RB0 = 1;
+    __delay_ms(1000);
+  }
 }
 
 
