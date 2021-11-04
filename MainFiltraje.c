c218c6fcb285ceaba4b116a8357b701f4212fbf2
EOrnelasR <91753324+EOrnelasR@users.noreply.github.com>
2021-11-03 22:53:43 -0600

    Update MainFiltraje.c
    Creacion de main

--- MainFiltraje.c
+++ MainFiltraje.c
@@ -14,8 +14,9 @@
 #include <conio.h>
 #include <string.h>
 
-//Asginacion de variables
-
+/*
+Asginacion de variables
+*/
 signal = [100];
 valor = 0;
 voltaje = 0;
@@ -23,9 +24,20 @@ ADC_Init();
 ADC_ON();
 ADC_AQ();
 ADCON0bits.GO_DONE = 1;
+/////////////////////////////////////////////////////////
+/*
+Asginacion de Funciones
+*/
+void config(void){
+  TRISBbits.RB0 = 1;
+}  
+//////////////////////////////////////////////////////////
 
-
-while(ADCON0bits.GO_DONE == 1){
-  valor = ADRES;
-  voltaje = (5.0*valor)/1023;
+void main(void){
+ while(ADCON0bits.GO_DONE == 1){
+    valor = ADRES;
+    voltaje = (5.0*valor)/1023;
+  } 
 }
+
+
